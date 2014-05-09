ccb28629289e0ae6630217cc9bcd851fca05eb22
Natapone C <natapone@gmail.com>
2014-05-10 00:07:27 +0700

    plot3

---
+++ plot3.R
@@ -0,0 +1,27 @@
+source("plot_utils.R")
+
+plot3 <- function () {
+    data <- read_data()
+    png(filename="plot3.png", width=480, height=480, bg="transparent" )
+    
+    plot(y=data$Sub_metering_1, 
+         x=data$Time,
+         type="l", 
+        ylab="Energy sub metering", 
+        xlab="",
+        col="black",
+    )
+    
+    lines(y=data$Sub_metering_2, x=data$Time, col="red")
+    lines(y=data$Sub_metering_3, x=data$Time, col="blue")
+    
+    legend("topright", inset=0, 
+        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+        lty=c(1,1,1),
+        lwd=c(2.5,2.5,2.5),
+        col=c("black","blue","red"),
+        horiz=F
+   )
+   
+   dev.off()
+}
